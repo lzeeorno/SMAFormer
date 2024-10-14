a2378739fac0bf56197fb54a211eaf9e400d505d
lzeeorno <43123195+lzeeorno@users.noreply.github.com>
2024-10-14 12:04:18 +0800

    Update train_synapse.py

--- train/train_synapse.py
+++ train/train_synapse.py
@@ -68,7 +68,7 @@ def parse_args():
     parser.add_argument('--early-stop', default=500, type=int,
                         metavar='N', help='early stopping (default: 30)')
     parser.add_argument('--gamma', default=1.0, type=float)
-    parser.add_argument('-b', '--batch_size', default=12, type=int,
+    parser.add_argument('-b', '--batch_size', default=16, type=int,
                         metavar='N', help='SMAFormer_LiTS_npy:12,SMAFormer_LiTS_png:12')
     parser.add_argument('--optimizer', default='SGD',
                         choices=['Adam', 'SGD'])
@@ -502,7 +502,7 @@ def main():
             ))
 
 
-            tmp1 = pd.Series([
+            tmp = pd.Series([
                 epoch,
                 train_log['lr'],
                 train_log['loss'],
@@ -533,10 +533,10 @@ def main():
                       'val_dice_7', 'val_dice_8','HD95_avg'])
 
             # 确保 log 的列顺序与 tmp 的索引顺序一致
-            log = log.reindex(columns=tmp1.index.tolist())
+            log = log.reindex(columns=tmp.index.tolist())
             # log = log._append(tmp, ignore_index=True)
             # 使用 pd.concat 替代 append
-            log = pd.concat([log, tmp1.to_frame().T], ignore_index=True)
+            log = pd.concat([log, tmp.to_frame().T], ignore_index=True)
             log.to_csv('../trained_models/{}_{}/{}/validation_{}_{}_{}_batchsize_{}.csv'.format(args.dataset, args.model_name, timestamp, args.model_name,
                                                                                  args.aug, args.loss, args.batch_size),index=True)
             print('save result to csv ->')
@@ -553,7 +553,7 @@ def main():
             #         print("=> early stopping")
             #         break
         else:
-            tmp2 = pd.Series([
+            tmp = pd.Series([
                 epoch,
                 train_log['lr'],
                 train_log['loss'],
@@ -566,15 +566,28 @@ def main():
                 train_log['dice_7'],
                 train_log['dice_8'],
                 # train_log['HD95'],
-            ], index=['epoch', 'lr', 'loss', 'dice_1', 'dice_2', 'dice_3'
-                      , 'dice_4', 'dice_5', 'dice_6', 'dice_7', 'dice_8'
-                      ])
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+                '',
+            ], index=['epoch', 'lr', 'loss', 'dice_1', 'dice_2', 'dice_3',
+                      'dice_4', 'dice_5', 'dice_6', 'dice_7', 'dice_8',
+                      'val_loss', 'val_iou', 'val_dice_1', 'val_dice_2',
+                      'val_dice_3', 'val_dice_4','val_dice_5', 'val_dice_6',
+                      'val_dice_7', 'val_dice_8','HD95_avg'])
 
             # # 确保 log 的列顺序与 tmp 的索引顺序一致
-            log = log.reindex(columns=tmp2.index.tolist())
+            log = log.reindex(columns=tmp.index.tolist())
             # log = log._append(tmp, ignore_index=True)
             # 使用 pd.concat 替代 append
-            log = pd.concat([log, tmp2.to_frame().T], ignore_index=True)
+            log = pd.concat([log, tmp.to_frame().T], ignore_index=True)
             log.to_csv('../trained_models/{}_{}/{}/Train_{}_{}_{}_batchsize_{}.csv'.format(args.dataset, args.model_name, timestamp,
                                                                                  args.model_name, args.aug, args.loss,
                                                                                  args.batch_size), index=True)
